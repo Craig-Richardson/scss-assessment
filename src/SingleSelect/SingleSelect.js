46ac2d6fb162957fc83f82d7e3859ff0b38e8dc6
Craig Richardson <thebrewhemoth@gmail.com>
2021-01-29 17:41:35 -0600

    In  Progress.

--- src/SingleSelect/SingleSelect.js
+++ src/SingleSelect/SingleSelect.js
@@ -12,6 +12,14 @@ const SingleSelect = props => {
             staging animations.
             make feedback a modal window.
 
+        Craig: It was interesting getting my desktop set up for all of this. I haven't previously worked
+        with Github, Yarn, or eslint, so figuring out how to fork into VS and use 'yarn start' was new 
+        and came with some errors that lead to a lot of troubleshooting and Googling. It mostly took a 
+        few command line adds, but it's all working, and I have the live version in a browser. This
+        process took me about 2 hours with a fresh VS install in that time.
+
+        Craig: Added class names to a few exiting divs to style them specifically, and added one div
+        around the buttons to help with styling.
     */
 
     const [selected, setSelected] = useState(-1);
@@ -24,15 +32,19 @@ const SingleSelect = props => {
 
     return (
         <div className={`SingleSelect`}>
-            <h1>
-                {props.data.questionText}
-            </h1>
-            {
-                selected === -1 &&
-                props.data.options.map((option, optionIndex) => {
-                    return <button onClick={()=>{handleSelect(optionIndex)}}>{option.text}</button>
-                })
-            }
+            <div className={'question'}>
+                <h1>
+                    {props.data.questionText}
+                </h1>
+            </div>
+            <div className={'answerButtons'}>
+                {
+                    selected === -1 &&
+                    props.data.options.map((option, optionIndex) => {
+                    return <button className={'answer'} onClick={()=>{handleSelect(optionIndex)}}>{option.text}</button>
+                    })
+                }
+            </div>
             {
                 selected > -1 &&
                 <div className={`feedback ${selectedOption.correct ? 'correct' : 'incorrect'}`}>
@@ -53,6 +65,7 @@ const SingleSelect = props => {
                     <button onClick={props.onComplete}>OK</button>
                 </div>
             }
+            <footer className={`footer`}></footer>
         </div>
     )
 }
